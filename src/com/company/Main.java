b60fb62b008e37ba2a062ca514f951835699bb4d
Eman <eman.cse2008@gmail.com>
2021-01-14 23:07:13 +0200

    filter distinct forEach

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -72,5 +72,13 @@ public class Main {
       })
       .limit(3)
       .collect(toList());
+
+    //filter
+    List<Dish> vegetarianDishes = menu.stream().filter(Dish::isVegetarian).collect(toList());
+
+    //filter unique elements
+    List<Integer> numbers = Arrays.asList(1,2,1,3,3,2,4);
+    numbers.stream().filter(i -> i%2 == 0).distinct().forEach(System.out::println);
+
   }
 }
