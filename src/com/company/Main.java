3521329d8d2e22730a4bcfd677d61f558f247036
Eman <eman.cse2008@gmail.com>
2021-01-14 21:39:16 +0200

    internal vs external iteration

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,18 @@ package com.company;
 
 import com.company.domain.Dish;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
     List<Dish> menu = Arrays.asList(
       new Dish("pork", false, 800, Dish.Type.MEAT),
       new Dish("beef", false, 700, Dish.Type.MEAT),
@@ -25,17 +28,37 @@ public class Main {
 
     List<String> threeHighCaloriesNames =
       menu.stream()
-      .filter(dish ->dish.getCalories() > 300)
-      .map(Dish::getName)
-      .limit(3)
-      .collect(toList());
+        .filter(dish -> dish.getCalories() > 300)
+        .map(Dish::getName)
+        .limit(3)
+        .collect(toList());
 
     System.out.println(threeHighCaloriesNames);
 
     //Traversable only once
-        List<String> title = Arrays.asList("Modern", "Java" , "In", "Action");
-        Stream<String> s = title.stream();
-        s.forEach(System.out::println);
+    List<String> title = Arrays.asList("Modern", "Java", "In", "Action");
+    Stream<String> s = title.stream();
+    s.forEach(System.out::println);
 //      s.forEach(System.out::println); will generate compilation error
+
+    //External vs Internal iterations
+    List<String> names1 = new ArrayList<>();
+    /* 1 - External iteration with for each */
+    for(Dish dish: menu){
+      names1.add(dish.getName());
     }
+    /* 2 - External iteration using an iterator */
+//    List<String> names2 = new ArrayList<>();
+//    Iterator<String> iterator = menu.iterator();
+//    while (iterator.hasNext()){
+//      Dish dish = iterator.next();
+//      names2.addAll(dish.getName());
+//    }
+//
+    /* 3 - Stream internal iteration */
+    List<String> names3 = menu.stream()
+      .map(Dish::getName)
+      .collect(toList());
+
+  }
 }
